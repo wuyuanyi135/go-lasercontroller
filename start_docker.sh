89eefa80df11944aa17666c22f3916b9c25b0df1
wuyuanyi <wuyuanyi135@gmail.com>
2018-12-14 20:03:44 -0500

    container should auto remove

--- start_docker.sh
+++ start_docker.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run -it --privileged -p 3050:3050 -v /dev:/dev wuyuanyi/laser_controller:latest
+docker run -it --rm --privileged -p 3050:3050 -v /dev:/dev wuyuanyi/laser_controller:latest
