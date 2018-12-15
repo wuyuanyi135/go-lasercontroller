c380e87d9f5567a8debad20158aa823d58af227e
wuyuanyi <wuyuanyi135@gmail.com>
2018-12-14 19:47:22 -0500

    reflection server

--- server/server.go
+++ server/server.go
@@ -3,13 +3,14 @@ package laserctrlgrpc
 import (
 	"github.com/wuyuanyi135/lasercontroller/protos"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 	"net"
 )
 
 func StartServer() {
 	grpcServer := grpc.NewServer()
 	laserctrlgrpc.RegisterLaserControlServiceServer(grpcServer, &LaserCtrlServer{})
-
+	reflection.Register(grpcServer)
 	lis, err := net.Listen("tcp", ":3050")
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
