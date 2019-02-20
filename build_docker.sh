995025797e90169230669390ea8b85227675996b
wuyuanyi <wuyuanyi135@gmail.com>
2019-02-20 16:39:47 -0500

    update proto version

--- build_docker.sh
+++ build_docker.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker build -f Dockerfile.amd64 --tag wuyuanyi/mvcamctrl:latest-amd64 .
-docker build -f Dockerfile.arm64 --tag wuyuanyi/mvcamctrl:latest-arm64v8 .
+docker build -f Dockerfile.amd64 --tag wuyuanyi/mvcamctrl:latest-amd64 -t microvision/mvcamctrl:latest-amd64 .
+docker build -f Dockerfile.arm64 --tag wuyuanyi/mvcamctrl:latest-arm64v8 -t microvision/mvcamctrl:latest-arm64v8 .
---
+++ push_docker.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+docker push microvision/mvcamctrl:latest-arm64v8
+docker push microvision/mvcamctrl:latest-amd64
